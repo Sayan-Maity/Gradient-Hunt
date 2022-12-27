74ad000e074b07828551d0446695f18bb976cf2e
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-28 02:50:52 +0530

    javascript files are added

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+import {colorList as c} from './colors.js'
+
+const boxes = document.querySelectorAll(".box");
+const copy_div = document.querySelector(".copy_div");
+const copy_msg = document.querySelector(".copy_msg");
+const code = document.querySelector("#code")
+
+let i = boxes.length;
+let j=0;
+for(j = 0; j < i; j++){
+    boxes[j].style.background = `linear-gradient(to right,${c[j]})`;
+};
+
+
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        let rgb = box.style.background;
+        navigator.clipboard.writeText(rgb);
+        code.innerHTML = rgb;
+        let bottom = -50;
+        copy_div.style.display = "Block";
+        let copy_interval = setInterval(() => {
+            copy_msg.style.bottom = `${bottom}vh`;
+            bottom += 1;
+            if(bottom>0){
+                clearInterval(copy_interval);
+                
+                setTimeout(() => {
+                    let bottom = 0;
+                    let copy_interval_two = setInterval(() =>{
+                        copy_msg.style.bottom = `${bottom}vh`;
+                        bottom -= 1;
+                        if(bottom < -50){
+                            clearInterval(copy_interval_two);
+                            copy_div.style.display = "none";
+
+                        }
+                    },1)
+                }, 2000);
+            }
+        },1);
+
+    });
+});
+
+
+
